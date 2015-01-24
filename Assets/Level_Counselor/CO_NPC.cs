f5cca97736444c8324fb674a21c53b041d426030
acholaghian <acholaghian@gmail.com>
2015-01-24 16:00:13 -0500

    4 PM Checkin, basic functionality + deleted excess scripts

---
+++ Assets/Level_Counselor/CO_NPC.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CO_NPC 
+{
+
+	private string[] NPCDialogue = new string[4];
+
+	public CO_NPC()
+	{
+		NPCDialogue[0] = "I got issues, doc. My brudda used ta feed my goldfish Goldfish crackas. I'm messed up man.";
+	}
+
+	public string GetDialogue(int choice)
+	{
+		return NPCDialogue[choice];
+	}
+}
