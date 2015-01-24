ea38ab52c9f5cc7387001dd3e9f1eb377367730d
Jonathan Rubinger <MeaninglessJargon@gmail.com>
2015-01-24 15:42:05 -0500

    Added simple CoderKey

--- Assets/Level_Programmer/Scripts/CoderKey.cs
+++ Assets/Level_Programmer/Scripts/CoderKey.cs
@@ -3,13 +3,44 @@ using System.Collections;
 
 public class CoderKey : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+	#region Public Methods
+	public void Activate () {
+		LightOn ();
+	}
+	public void Deactivate () {
+		LightOff ();
+	}
+	#endregion
 	
+	
+	private void LightOn () {
+		renderer.enabled = true;
+	}
+	private void LightOff () {
+		renderer.enabled = false;
+	}
+	private bool IsActive {
+		get { return renderer.enabled; }
+	}
+	private void PressKey () {
+		
+		if (IsActive) {
+			// Send keyboard a success message
+		}
+		else {
+			// Send keyboard a failure message
+		}
+	}
+		
+	#region Events
+	void OnMouseDown () {
+		CoderWeapon.HitKey(transform);
 	}
 	
-	// Update is called once per frame
-	void Update () {
-	
+	void OnTriggerEnter (Collider other) {
+		if (other.gameObject.layer == LayerMask.NameToLayer("KeyPresser")) {
+			PressKey();
+		}
 	}
+	#endregion
 }
