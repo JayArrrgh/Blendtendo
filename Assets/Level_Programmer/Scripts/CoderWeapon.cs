500ae335d2d037e4b7c4ffb79f9c9b2d3b57c687
Jonathan Rubinger <MeaninglessJargon@gmail.com>
2015-01-24 15:41:32 -0500

    Added CoderWeapon for hitting keys

---
+++ Assets/Level_Programmer/Scripts/CoderWeapon.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CoderWeapon : MonoBehaviour {
+	
+	private static CoderWeapon instance;
+	
+	void Awake () {
+		instance = this;
+	}
+	
+	#region Public methods
+	public static void HitKey(Transform key) {
+		print ("Hitting key: " + key.name);
+	}
+	#endregion
+}
