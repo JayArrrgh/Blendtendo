c18be965c24f6209378fafd8e02ef08264dc04a3
Jonathan Rubinger <MeaninglessJargon@gmail.com>
2015-01-24 13:11:30 -0500

    Switched asset serialization to "Force Text"

---
+++ Assets/Level_Programmer/CoderOutput.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CoderOutput : MonoBehaviour {
+
+	public string[] codeLines;
+	public string errorLine;
+	
+	private GUIText output;
+	private string current = "";
+	
+	void Start () {
+		output = guiText;
+		output.text = "";
+		InvokeRepeating("PrintCharacter", 0f, 0.1f);
+	}
+	
+	public void PrintLine () {
+		
+	}
+	
+	public void PrintError () {
+		
+	}
+	
+	private void PrintCharacter () {
+		if (current != output.text) {
+			output.text += current.Substring(output.text.Length, 1);
+		}
+	}
+}
