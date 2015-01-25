52611f5549306cc8bfe7e16bee18ac9ce89a27ff
mhrooy <mhrooy@gmail.com>
2015-01-25 14:54:57 -0500

    xfhsxgf

---
+++ Assets/Level_MainMenu/Scripts/LookForward.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LookForward : MonoBehaviour {
+
+	public Transform sightStart, sightEnd;
+	public bool needsCollision = true;
+
+	private bool collision = false;
+	private Animator animator;
+
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        collision = Physics2D.Linecast(sightStart.position, sightEnd.position, 1 << LayerMask.NameToLayer("Solid"));
+        Debug.DrawLine(sightStart.position, sightEnd.position, Color.green);
+
+        if (collision == needsCollision){
+            //directionX = 0;
+            animator.SetInteger("AnimState", 0);
+        }
+	}
+}
