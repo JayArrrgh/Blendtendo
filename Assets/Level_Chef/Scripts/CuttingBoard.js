44af442e3993389b6742ad9b049a6f629d73194b
puzzud <puzzud@gmail.com>
2015-01-25 13:44:54 -0500

    Chef - Added sound files. Added way to quite Chef game and return to main menu.

---
+++ Assets/Level_Chef/Scripts/CuttingBoard.js
@@ -0,0 +1,37 @@
+﻿#pragma strict
+
+public class CuttingBoard extends MonoBehaviour
+{
+  function Start()
+  {
+    
+  }
+
+  function Update()
+  {
+    
+  }
+  
+  function OnTriggerEnter2D( other: Collider2D )
+  {
+    var otherName : String = other.name;
+    if( otherName.Contains( "Chicken" ) ) // NOTE: This is super sensitive bad coding!
+    {
+      //print( otherName );
+      var food : Food = other.GetComponent( Food );
+      if( food != null )
+      {
+        // Determine if food is raw, cooked, or overcooked.
+        
+        // Update user score.
+        
+        // Move chicken back to initial point.
+      }
+    }
+  }
+  
+  function OnTriggerExit2D( other : Collider2D )
+  {
+    //associate( null );
+  }
+}
